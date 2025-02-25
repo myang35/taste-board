94213ba5ab3966b1602497c2c91da90f28d93174
Marvin Yang <marvinyangs@gmail.com>
2025-02-25 00:00:54 -0500

    feat: setup theme and page layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './core/ui/footer/footer.component';
+import { HeaderComponent } from './core/ui/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
