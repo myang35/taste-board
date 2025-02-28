2c5591cc9e760a15af0e274046d7d47b8fd9341d
garrison <gkbolick@yahoo.com>
2025-02-28 00:20:59 -0500

    feat: log in page with signup template added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { LoginComponent } from './features/login/login.component';
+import { SignUpComponent } from './features/sign-up/sign-up.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
+    component: HomeComponent, 
   },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent,
+  }
 ];
