28ae2dd747b693b9e7cb5acf8a27eb7f23249886
Marvin Yang <marvinyangs@gmail.com>
2025-03-14 03:58:41 -0400

    chore: move auth service files into an auth folder

--- src/app/core/interceptors/auth/auth.interceptor.ts
+++ src/app/core/interceptors/auth/auth.interceptor.ts
@@ -1,6 +1,6 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { AuthService } from '@core/services/auth.service';
+import { AuthService } from '@core/services/auth/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
