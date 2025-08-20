d9b3f60944bec75aba9046100217a7f0d42ca5ef
Marvin Yang <marvinyangs@gmail.com>
2025-08-20 17:26:30 -0400

    feat: overhaul style colors and design header, footer, and home page

--- src/app/core/ui/footer/footer.component.ts
+++ src/app/core/ui/footer/footer.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
-export class FooterComponent {
-
-}
+export class FooterComponent {}
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
   constructor(private router: Router) {}
 
   navigateToLogin() {
