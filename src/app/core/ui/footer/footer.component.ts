ee60086af4c2a93648904138f86e047315662a80
Marvin Yang <marvinyangs@gmail.com>
2025-10-08 03:25:26 -0400

    feat(footer): change Account section based on if user is logged in

--- src/app/core/ui/footer/footer.component.ts
+++ src/app/core/ui/footer/footer.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-import { MatIcon } from '@angular/material/icon';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '@core/services/auth/auth.service';
 
 @Component({
   selector: 'app-footer',
-  imports: [MatIcon],
+  imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  private readonly authService = inject(AuthService);
+
+  protected user = this.authService.user;
+}
